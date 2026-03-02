14bc3fe9bad4b897a75100b233af48c8c894ffab
solouza <nabilvhr1908@gmail.com>
2026-03-03 00:12:20 +0700

    Initial commit

---
+++ Assets/Scenes/navmesh.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemychasing : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
